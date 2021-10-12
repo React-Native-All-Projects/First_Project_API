6dbdfa3f5daf61a8cdbddb822b2707a5502cedd8
OsamaTahaluf <91789466+OsamaTahaluf@users.noreply.github.com>
2021-10-12 11:26:29 +0100

    Initial Create

---
+++ First_Project_API/App.js
@@ -0,0 +1,130 @@
+import React from "react";
+
+import { View,Text } from "react-native";
+
+const App = () =>{
+
+    //------------------------------------------------------------------------Get ***********************************
+    const Get = async () => {
+    try {
+      let response = await fetch('http://073b-81-28-126-167.ngrok.io/api/Shared/Get');
+      let json = await response.json();
+          console.log( 'Get');
+      console.log(json);
+    } catch (error) {
+      console.error(error);
+    }}
+
+    Get();
+
+    const GetWithURL = async (data) => {
+    try {
+      let response = await fetch('http://073b-81-28-126-167.ngrok.io/api/Shared/GetWithURL/'+data);
+      let json = await response.json();
+      console.log( 'Get With URL');
+      console.log(json);
+    } catch (error) {
+      console.error(error);
+    }}
+
+     GetWithURL('My Data');
+
+
+
+    //------------------------------------------------------------------------Post ****************************************
+    const Post = async () => {
+        await fetch('http://073b-81-28-126-167.ngrok.io/api/Shared/Put',{
+          method:'PUT',
+        }).then(async (response) =>{
+            let json = await response.json();
+              console.log('Post');
+            console.log(json);
+        })
+
+  };
+  Post();
+
+  //------------------------------------------------------------------------Post With Body
+    const PostWithBody = async () => {
+        await fetch('http://073b-81-28-126-167.ngrok.io/api/Shared/PutWithBody',{
+          method:'PUT',
+            headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  },
+           body: JSON.stringify({
+    UserName : 'SuperUser',
+    Password :'SuperUser'
+      }),
+        }).then(async (response) =>{
+          let json = await response.json();
+            console.log('Post With Body');
+          console.log(json);
+        })
+  };
+
+  PostWithBody();
+
+
+
+
+
+
+  //------------------------------------------------------------------------Put ****************************************
+    const Put = async () => {
+        await fetch('http://073b-81-28-126-167.ngrok.io/api/Shared/Put',{
+          method:'PUT',
+        }).then(async (response) =>{
+            let json = await response.json();
+              console.log('Put');
+            console.log(json);
+        })
+
+  };
+  Put();
+
+  //------------------------------------------------------------------------Put With Body
+    const PutWithBody = async () => {
+        await fetch('http://073b-81-28-126-167.ngrok.io/api/Shared/PutWithBody',{
+          method:'PUT',
+            headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  },
+           body: JSON.stringify({
+    UserName : 'SuperUser',
+    Password :'SuperUser'
+      }),
+        }).then(async (response) =>{
+          let json = await response.json();
+            console.log('Put With Body');
+          console.log(json);
+        })
+  };
+
+  PutWithBody();
+
+
+    //------------------------------------------------------------------------Delete *********************************
+    const DeleteWithURL = async (Id) => {
+        await fetch('http://073b-81-28-126-167.ngrok.io/api/Shared/DeleteWithURL/'+Id,{
+          method:'Delete',
+        }).then(async (response) =>{
+          let json = await response.json();
+            console.log('Delete With URL');
+          console.log(json);
+        })
+  };
+
+  DeleteWithURL(1);
+
+
+
+  return (
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App;
